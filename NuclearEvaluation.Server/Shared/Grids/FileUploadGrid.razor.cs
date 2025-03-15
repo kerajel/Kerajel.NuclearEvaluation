bcefdaacb4b4ccdf53044f76c9eb05d34ab7676a
Dmitrii Botov <dmitrii.botov@rokolabs.com>
2025-03-15 16:23:28 +0400

    FileUploadGrid to separate component

---
+++ NuclearEvaluation.Server/Shared/Grids/FileUploadGrid.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using NuclearEvaluation.Server.Models.Upload;
+
+namespace NuclearEvaluation.Server.Shared.Grids;
+
+public partial class FileUploadGrid
+{
+    [Parameter]
+    public IEnumerable<UploadedFile> Data { get; set; } = Enumerable.Empty<UploadedFile>();
+
+    [Parameter]
+    public EventCallback<UploadedFile> OnRemoveFile { get; set; }
+
+    private async Task RemoveFile(UploadedFile file)
+    {
+        if (OnRemoveFile.HasDelegate)
+        {
+            await OnRemoveFile.InvokeAsync(file);
+        }
+    }
+}
